9a061308f0727e342f483e1d50ebd1b25fef5a05
ojasiron <65645293+ojasiron@users.noreply.github.com>
2020-11-19 19:32:11 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+var dog , happyDog,  database ,foodS,foodStock;
+function preload()
+{
+  dog1=loadImage("images/Dog.png");
+  happyDog= loadImage("images/happydog.png");
+}
+
+function setup() {
+	createCanvas(500, 500);
+    database = firebase.database();
+   dog = createSprite(250,250);
+  dog.addImage(dog1);
+  dog.scale = 0.6;
+  
+    foodStack=database.ref('food');
+    foodStack.on("value",readStock);
+  
+  
+}
+
+function readStock(data){
+    foodS = data.val()
+}
+function draw() {  
+background(46, 139, 87);
+  drawSprites();
+  //add styles here
+  fill("black");
+  text(foodS,250,50);
+  if(keyDown(UP_ARROW)&&foodS>0){
+console.log("keyDown")
+dog.addImage(happyDog)
+foodS--;
+database.ref('/').update({
+    food:foodS
+})
+  }
+  
+}
+
+
+
